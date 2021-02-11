f26047a6b5163303c7192bb7375251f2dcd944d5
cysco-byte <cryptoescor@protonmail.com>
2021-02-11 15:37:46 -0300

    added tests/completed routes

--- routes/api.js
+++ routes/api.js
@@ -15,22 +15,40 @@ module.exports = function (app) {
   let convertHandler = new ConvertHandler();
 
   app.route("/api/convert").get(function (req, res) {
-    let input = req.query.input;
-    let initNum = convertHandler.getNum(input);
-    let initUnit = convertHandler.getUnit(input);
-    let returnNum = convertHandler.convert(initNum, initUnit);
-    let returnUnit = convertHandler.getReturnUnit(initUnit);
-    let toString = convertHandler.getString(
+    let input = req.query.input.toLowerCase();
+    try {
+      convertHandler.checkNIErr(input);
+    } catch (e) {
+      res.send("invalid number and input");
+    }
+    try {
+      convertHandler.getNum(input);
+    } catch (e) {
+      res.send("invalid number");
+    }
+    try {
+      convertHandler.getUnit(input);
+    } catch (e) {
+      res.send("invalid unit");
+    }
+
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.spellOutUnit(input);
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const string = convertHandler.getString(
       initNum,
       initUnit,
       returnNum,
       returnUnit
     );
+
     res.json({
       initNum: initNum,
       initUnit: initUnit,
       returnNum: returnNum,
       returnUnit: returnUnit,
+      string: string,
     });
   });
 };
